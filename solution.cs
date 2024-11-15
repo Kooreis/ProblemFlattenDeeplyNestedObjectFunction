577b8e532c89f81a5fe60afb3e5d47927d262dc3
Kooreis <kooreis.official@gmail.com>
2024-11-15 02:40:38 -0600

    This is the definition of the function 'flattenObject'. It takes a nested object and returns a new object where each key is a path to the original value in the nested object. The function uses recursion to traverse the nested object.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a JavaScript solution using recursion:
-
-```javascript
 function flattenObject(obj, parent, res = {}) {
     for(let key in obj) {
         let propName = parent ? parent + '.' + key : key;
@@ -12,22 +9,3 @@ function flattenObject(obj, parent, res = {}) {
     }
     return res;
 }
-
-// Test the function
-let nestedObject = {
-    a: 1,
-    b: {
-        c: 2,
-        d: {
-            e: 3,
-            f: {
-                g: 4
-            }
-        }
-    }
-};
-
-console.log(flattenObject(nestedObject));
-```
-
-This script defines a function `flattenObject` that takes a nested object and returns a new object where each key is a path to the original value in the nested object. The function uses recursion to traverse the nested object. The `flattenObject` function is then tested with a deeply nested object. The output of the script is a flattened version of the nested object.
